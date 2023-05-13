ac79b3d665aee13da3ba607cb2b05bd8d9148049
Javier <javier.sanz@estudiantat.upc.edu>
2023-05-13 12:23:06 +0200

    home screen

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,25 +1,62 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import cinema from "./cinema.jpg";
-import { Row, Col } from 'react-bootstrap';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+// import cinema from "./cinema.jpg";
+// import { Row, Col } from 'react-bootstrap';
+import './Home.css';
+import music from "./music.jpg";
+import music2 from "./music2.jpg";
+import { Link } from 'react-router-dom';
 
 const Home = () => (
-  <Row>
-  <Col md={3}>
-      <Card>
-        <Card.Img variant="top" src={cinema} />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Subtitle>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </Col>
-  </Row>
+
+  <div className='card-container' >
+      {/* Card 1 */}
+      <Link to="/music" className="card" style={{ backgroundImage: `url(${music2})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+      <div className="card-content" >
+          <p>Music</p>
+        </div>
+      </Link>
+      
+      {/* Card 2 */}
+      <Link to="/videos" className="card" style={{ backgroundImage: `url(${music2})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+      <div className="card-content" >
+          <p>Videos</p>
+        </div>
+      </Link>
+      
+      {/* Card 3 */}
+      <Link to="/games" className="card" style={{ backgroundImage: `url(${music2})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+      <div className="card-content" >
+          <p>Games</p>
+        </div>
+      </Link>
+      {/* Card 4 */}
+      <Link to="/destination" className="card" style={{ backgroundImage: `url(${music2})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+      <div className="card-content" >
+          <p>Destination</p>
+        </div>
+      </Link>
+
+    </div>
+
 );
 
 export default Home
+
+
+  // <Row>
+  // <Col md={3}>
+  //     <Card>
+  //       <Card.Img variant="top" src={cinema} />
+  //       <Card.Body>
+  //         <Card.Title>Card Title</Card.Title>
+  //         <Card.Subtitle>
+  //           Some quick example text to build on the card title and make up the
+  //           bulk of the card's content.
+  //         </Card.Subtitle>
+  //         <Button variant="primary">Go somewhere</Button>
+  //       </Card.Body>
+  //     </Card>
+  //   </Col>
+  // </Row>
