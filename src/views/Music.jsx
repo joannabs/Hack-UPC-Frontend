550a52ce6831d47fc5c78761b75f72fa225ecfe3
Javier <javier.sanz@estudiantat.upc.edu>
2023-05-13 16:00:50 +0200

    music v1 films v1

--- src/views/Music.jsx
+++ src/views/Music.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Music.css';
 
-const Music = () => (
-  <section className="Home">
-    <h3>Hello Music</h3>
-  </section>
-)
+const Songs = () => {
+  const initialSongs = [
+    { id: 1, name: 'Song 1', votes: 0 },
+    { id: 2, name: 'Song 2', votes: 0 },
+    { id: 3, name: 'Song 3', votes: 0 },
+  ];
 
-export default Music;
+  const [songs, setSongs] = useState(initialSongs);
+
+  const handleVoteClick = (id) => {
+    const updatedSongs = songs.map((song) => {
+      if (song.id === id) {
+        return { ...song, votes: song.votes + 1, voted: true };
+      }
+      return song;
+    });
+    setSongs(updatedSongs);
+  };
+
+  return (
+    <div className="songs">
+      <h1>Songs</h1>
+      {songs.map((song) => (
+        <SongCard
+          key={song.id}
+          name={song.name}
+          votes={song.votes}
+          voted={song.voted}
+          onVoteClick={() => handleVoteClick(song.id)}
+        />
+      ))}
+    </div>
+  );
+};
+
+const SongCard = ({ name, votes, voted, onVoteClick }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayClick = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <>
+    <div className="song-card">
+       <h3>{name}</h3>
+      <p>Votes: {votes}</p>
+      {!voted && (
+        <div className="buttons">
+          <button className="vote-button" onClick={onVoteClick}>
+            Vote
+          </button>
+        </div>
+      )}
+        <button className="play-button" onClick={handlePlayClick}>
+          {isPlaying ? 'Delete from playlist' : 'Add to playlist'}
+        </button>
+      </div>
+    </>
+  );
+};
+
+
+export default Songs;
