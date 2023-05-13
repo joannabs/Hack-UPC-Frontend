58d65513762584831cc565a42f00b781990d3601
jboronats <jboronats@minsait.com>
2023-05-14 01:46:00 +0200

    movie screen

--- src/views/Films.jsx
+++ src/views/Films.jsx
@@ -1,69 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import './Films.css';
+import ReactPlayer from 'react-player';
+import logo from './Enterflightment.png';
 
 const Films = () => {
+  const [videoUrl, setVideoUrl] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('http://10.5.237.7:8080/video/1')
+      .then(response => response.blob())
+      .then(data => {
+        const videoBlob = new Blob([data], { type: 'video/mp4' });
+        const videoUrl = URL.createObjectURL(videoBlob);
+        setVideoUrl(videoUrl);
+      })
+      .catch(error => {
+        console.error('Error fetching video data:', error);
+      })
+      .finally(() => {
+        setLoading(false); // Set loading state to false after the API call is complete
+      });
+  }, []);
+
   return (
     <>
-    <Navbar/>
-    <div className="films">
-      <h1>Films</h1>
-      <h4>Ice Age 1</h4>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/AM2qPVypa9M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <br></br>
-      <h4>Terminator</h4>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/Vj9vPLXFhfc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>    </div>
+      <div>
+        <img className="logo" src={logo} />
+      </div>
+      <Navbar />
+      <h1 className='film'>Films</h1>
+      <h1 className='film1'>Trip's film: Shrek</h1>
+      <div className="player-container">
+        {loading ? (
+          <div className="loading-spinner">
+            <div className="spinner"></div>
+          </div>
+        ) : (
+          videoUrl && (
+            <ReactPlayer url={videoUrl} controls width="40%" height="auto" />
+          )
+        )}
+      </div>
     </>
   );
 };
 
 export default Films;
-
-
-
-// import React, { useEffect, useState } from 'react';
-// import Navbar from '../components/Navbar/Navbar';
-// import './Films.css';
-// const Films = () => {
-//   const [films, setFilms] = useState([]);
-//   const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=tt3398228&source=imdb&country=us';
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-//       'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
-//     }
-//   };
-  
-
-//   useEffect(() => {
-//     // Simulating API call to fetch films data
-//     fetch(url, options)
-//       .then(response => response.json())
-//       .then(data => setFilms(data))
-//       .catch(error => console.log(error));
-//   }, []);
-
-//   return (
-//     <>
-//      <Navbar/>
-//     <div className="films">
-//       <h1>Films</h1>
-      
-//       {/* {films.map(film => (
-//         <div key={film.id} className="film">
-//           <h2>{film.title}</h2>
-//           <h3>People:</h3>
-//           <ul>
-//             {film.people.map(person => (
-//               <li key={person.id}>{person.name}</li>
-//             ))}
-//           </ul>
-//         </div>
-//       ))} */}
-//     </div>
-//     </>
-//   );
-// };
-
-// export default Films;
