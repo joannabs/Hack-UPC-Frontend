38f1309aa131064f42d07783fe069c9d42b495ca
jboronats <jboronats@minsait.com>
2023-05-13 12:27:00 +0200

    navbar decoration

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import cinema from "./cinema.jpg";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container, Card, CardImg } from 'react-bootstrap';
 
 const Home = () => (
-  <Row>
-  <Col md={3}>
-      <Card>
-        <Card.Img variant="top" src={cinema} />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Subtitle>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </Col>
-  </Row>
+  <Container className='p-4'> 
+  <Row> 
+    <Col md="4">  
+      <Card>  
+        <Card.Img variant="top" src={cinema} />  
+        <Card.Body>  
+          <Card.Title>Card Title</Card.Title>  
+          <Card.Text>  
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae molestie magna. Vivamus sed molestie enim, eu convallis mauris. Aliquam pharetra velit ac enim maximus, a commodo augue hendrerit. Phasellus at aliquam est  
+          </Card.Text>  
+          <Button variant="primary">Read More</Button>  
+        </Card.Body>  
+      </Card>  
+    </Col> 
+  </Row> 
+  </Container>  
 );
 
 export default Home
